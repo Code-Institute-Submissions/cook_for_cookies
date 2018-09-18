11073ee94b7dd9d7a9c11ab30f43cb49bd6673af
Giancarlo Fiorletta <caianroma@yahoo.co.uk>
2018-09-18 09:05:41 +0000

    Added basic home route and html plus css sheet and skeleton nav

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for, request
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = "onlinecookbook"
+app.config["MONGO_URI"] = "mongodb://admin:Sameday123@ds161112.mlab.com:61112/onlinecookbook"
+
+mongo = PyMongo(app)
+
 @app.route('/')
 def home():
-    return 'Hello World'
+    return render_template("home.html", users = mongo.db.Users.find())
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
